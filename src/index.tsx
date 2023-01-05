9dbf2bca8c1dab4d30d646fe28d845729ece9086
Ivo Toby <ivo.toby@contentful.com>
2023-01-05 16:40:23 +0100

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import { GlobalStyles } from '@contentful/f36-components';
-import { SDKProvider } from '@contentful/react-apps-toolkit';
+import { GlobalStyles } from "@contentful/f36-components";
+import { SDKProvider } from "@contentful/react-apps-toolkit";
 
-import LocalhostWarning from './components/LocalhostWarning';
-import App from './App';
+import LocalhostWarning from "./components/LocalhostWarning";
+import App from "./App";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
-if (process.env.NODE_ENV === 'development' && window.self === window.top) {
+if (process.env.NODE_ENV === "development" && window.self === window.top) {
   // You can remove this if block before deploying your app
   render(<LocalhostWarning />, root);
 } else {
   render(
     <SDKProvider>
       <GlobalStyles />
+      <p>Yolo</p>
+
       <App />
     </SDKProvider>,
     root
